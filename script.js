fc7050a14e003ae8cfb0a5ac2f89f01c9018c461
Pete Kaffenberger <pete.kaffenberger@gmail.com>
2021-04-04 20:41:58 -0400

    transposed README.md format, working on local storage

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-var hours = {
-    "9": "",
-    "10": "",
-    "11": "",
-    "12": "",
-    "1": "",
-    "2": "",
-    "3": "",
-    "4": "",
-    "5": "",
-}
-
-var timeDisplayEl = $('#time-display');
 
 
-function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY');
-    timeDisplayEl.text(rightNow);
-  }
-setInterval(displayTime, 1000);
+$(document).ready(() => {
+
+        var hours = {
+            "9": "",
+            "10": "",
+            "11": "",
+            "12": "",
+            "1": "",
+            "2": "",
+            "3": "",
+            "4": "",
+            "5": "",
+        }
+
+        var timeDisplayEl = $('#time-display');
+
+
+        function displayTime() {
+            var rightNow = moment().format('MMM DD, YYYY');
+            timeDisplayEl.text(rightNow);
+        }
+        setInterval(displayTime, 1000);
+
+        $("#btn9)").click(function () {
+            var content = $("#hour9").val();
+            hours[9] = content;
+            localStorage.setItem(9, JSON.stringify(content));
+        });
+
+        var storedData = JSON.parse(window.localStorage.getItem(9))
+        $("#hour9").val(storedData);
+    
+    
+    })
